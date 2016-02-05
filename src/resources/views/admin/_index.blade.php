1582b27ef213cdccdb58a122da3f357ddede7967
Samuel De Backer <sdebacker@gmail.com>
2016-02-05 23:05:16 +0100

    list with vue.js

--- src/resources/views/admin/_index.blade.php
+++ src/resources/views/admin/_index.blade.php
@@ -1,8 +1,12 @@
-<div ng-app="typicms" ng-cloak ng-controller="ListController">
+<script src="http://cdn.jsdelivr.net/vue/1.0.14/vue.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script src="https://raw.githubusercontent.com/matfish2/vue-tables/master/dist/vue-tables.min.js"></script>
+
+<div id="news">
 
     <a href="{{ route('admin.' . $module . '.create') }}" class="btn-add"><i class="fa fa-plus-circle"></i><span class="sr-only">New</span></a>
     <h1>
-        <span>@{{ models.length }} @choice('news::global.news', 2)</span>
+        <span>@{{ tableData.length }} @choice('news::global.news', 2)</span>
     </h1>
 
     <div class="btn-toolbar">
@@ -10,46 +14,59 @@
     </div>
 
     <div class="table-responsive">
-
-        <table st-persist="newsTable" st-table="displayedModels" st-safe-src="models" st-order st-filter class="table table-condensed table-main">
-            <thead>
-                <tr>
-                    <th class="delete"></th>
-                    <th class="edit"></th>
-                    <th st-sort="status" class="status st-sort">Status</th>
-                    <th st-sort="image" class="image st-sort">Image</th>
-                    <th st-sort="date" st-sort-default="reverse" class="date st-sort">Date</th>
-                    <th st-sort="title" class="title st-sort">Title</th>
-                </tr>
-                <tr>
-                    <td colspan="5"></td>
-                    <td>
-                        <input st-search="title" class="form-control input-sm" placeholder="@lang('global.Search')…" type="text">
-                    </td>
-                </tr>
-            </thead>
-
-            <tbody>
-                <tr ng-repeat="model in displayedModels">
-                    <td typi-btn-delete action="delete(model)"></td>
-                    <td>
-                        @include('core::admin._button-edit')
-                    </td>
-                    <td typi-btn-status action="toggleStatus(model)" model="model"></td>
-                    <td>
-                        <img ng-src="@{{ model.thumb }}" alt="">
-                    </td>
-                    <td>@{{ model.date | dateFromMySQL:'dd/MM/yyyy' }}</td>
-                    <td>@{{ model.title }}</td>
-                </tr>
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="6" typi-pagination></td>
-                </tr>
-            </tfoot>
-        </table>
-
+      <v-client-table :data="tableData" :options="options"></v-client-table>
     </div>
 
 </div>
+
+<script>
+    Vue.use(VueTables.client, {
+        compileTemplates: true,
+        //highlightMatches: true,
+        pagination: {
+            // dropdown:true
+            // chunk:5
+        },
+        //filterByColumn: true,
+        texts: {
+            filter: "@lang('global.Search')"
+        },
+        skin:'table-condensed'
+    });
+
+    new Vue({
+        el: "#news",
+        methods: {
+            deleteMe: function (id) {
+                alert("Delete " + id);
+            }
+        },
+        data: {
+            tableData: {!! News::all([], true) !!},
+            options: {
+                columns: ['id', 'status', 'thumb', 'date', 'title'],
+                dateFormat: 'yyyy mm dd',
+                perPage: 25,
+                perPageValues: [25, 50, 100, 500, 1000, 5000],
+                headings: {
+                    id: '',
+                    status: 'Status',
+                    thumb: 'Image',
+                    date: 'Date',
+                    title: 'Title'
+                },
+                templates: {
+                    id: '<a href="javascript:void(0);" @click="$parent.deleteMe({id})"><span class="fa fa-remove"></span></a>&nbsp;&nbsp;&nbsp;<a class="btn btn-default btn-xs" href="news/{id}/edit">Edit</i></a>',
+                    status: '<div class="btn btn-xs btn-link" @click="action()">' +
+                        '<span class="fa switch" :class="{status} ? \'fa-toggle-on\' : \'fa-toggle-off\'"></span>' +
+                    '</div>',
+                    thumb: '<img src="{thumb}">'
+                },
+                orderBy: {
+                    column: 'date',
+                    ascending: false
+                }
+            }
+        }
+    });
+</script>
